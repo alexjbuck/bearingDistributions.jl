aa0f9cb8cb3da72d79d558d93847018447a7acbb
Alex Buck <alexjbuck@gmail.com>
2020-12-12 17:15:11 -0800

    First plotting PDFs
    plots PDF for intersection of two lines

--- src/bearingDistributions.jl
+++ src/bearingDistributions.jl
@@ -1,5 +1,84 @@
 module bearingDistributions
+using Distributions
+using LinearAlgebra
+using Plots
 
-# Write your package code here.
+export Bearing, intersectGrid
+
+"""
+Bearing object.
+
+Defined by a mean direction θ, a standard deviation of direction σ, and a position (x,y).
+"""
+struct Bearing
+    θ
+    σ
+    x
+    y
+end
+
+"""
+y = y1 + x*tan(θ1) - x1*tan(θ2)
+y = y2 + x*tan(θ2) - x2*tan(θ2)
+
+y - x*tan(θ1) = y1 - x1*tan(θ1)
+y - x*tan(θ2) = y2 - x2*tan(θ2)
+
+[-tan(θ1) 1]   [x]  = [y1 - x1*tan(θ1)]
+[-tan(θ2) 1] * [y]  = [y2 - x2*tan(θ2)]
+
+[x] = inv( [-tan(θ1) 1] ) * [y1 - x1*tan(θ1)]
+[y] =      [-tan(θ2) 1]     [y2 - x2*tan(θ2)]
+
+"""
+function findIntersect(b1::Bearing, b2::Bearing)
+    A = [-tan(b1.θ) 1;
+         -tan(b2.θ) 1];
+
+    B = [(b1.y - b1.x*tan(b1.θ));
+         (b2.y - b2.x*tan(b2.θ))]
+
+    point = inv(A) * B;
+end
+
+function boundingBox(b1::Bearing, b2::Bearing)
+    pᵢ = findIntersect(b1, b2);
+    p₁ = [b1.x;b1.y];
+    p₂ = [b2.x;b2.y];
+    d₁ = pᵢ - p₁;
+    d₂ = pᵢ - p₂;
+    D₁ = norm(d₁);
+    D₂ = norm(d₂);
+    D = max(D₁, D₂);
+    box = pᵢ .+ 2*[-D D;-D D];
+end
+
+function rangeFromBox(box::Array{Number,2}; length = 51)
+    x,y = rangeFromBox(box...; length)
+    return x,y
+end
+
+function rangeFromBox(x1,y1,x2,y2; length)
+    x = range(x1, x2; length);
+    y = range(y1, y2; length);
+    return x,y
+end
+
+function probabilityGrid(b::Bearing, x::AbstractArray, y::AbstractArray)
+    D = Normal(b.θ, b.σ);
+    Θ = atan.(y .- b.y, x' .- b.x);
+    P = pdf.(D, Θ);
+    return x, y, P
+end
+
+
+function intersectGrid(b1::Bearing, b2::Bearing; length = 51)
+    box = boundingBox(b1, b2);
+    x,y = rangeFromBox(box...; length)
+    _,_,P₁ = probabilityGrid(b1, x, y);
+    _,_,P₂ = probabilityGrid(b2, x, y);
+    Pᵢ = P₁ .* P₂
+    return x, y, Pᵢ
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
 using bearingDistributions
+using Plots
 using Test
 
+const bd = bearingDistributions
+
 @testset "bearingDistributions.jl" begin
-    # Write your tests here.
+    b1 = Bearing(atan(1,1),  .1, 0, 0);
+    b2 = Bearing(atan(-1,1), .2, 0, 1);
+    @test b1.x ≈ 0
+    @test b1.y ≈ 0
+    @test b1.θ ≈ atan(1,1)
+    @test b1.σ ≈ .1
+    @test b2.x ≈ 0
+    @test b2.y ≈ 1
+    @test b2.θ ≈ atan(-1,1)
+    @test b2.σ ≈ .2
+    @test bd.findIntersect(b1, b2) ≈ [.5;.5] 
+
+    b1 = Bearing(0, 1, 0, 1);
+    b2 = Bearing(π/2, .1, 1, 0);
+    @test bd.boundingBox(b1, b2) ≈ [-1 3; -1 3]
+
+    b1 = Bearing(0, .2, 0, 1);
+    b2 = Bearing(π/2, .2, 1, 0);
+    box = bd.boundingBox(b1, b2);
+    x,y = bd.rangeFromBox(box...; length = 51)
+
+    contour(bd.probabilityGrid(b1,x,y)...)
+    contour(bd.probabilityGrid(b2,x,y)...)
+    contour(intersectGrid(b1, b2; length = 51)...)
 end
